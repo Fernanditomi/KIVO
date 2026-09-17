6b5832c824ade0cd9aeb04f78b11c75e0a739ce6
Fernanditomi <Fernanditomi@users.noreply.github.com>
2026-09-16 21:24:00 -0600

    chore: bump version a 1.3.0 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.kivo"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
